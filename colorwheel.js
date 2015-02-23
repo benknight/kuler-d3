35d9d28409f4226efe6ca7d5093c05dd7e62cf72
Benjamin Knight <ben@benknight.me>
2015-02-23 10:20:38 -0800

    Breaking things into plugins I guess

--- colorwheel.js
+++ colorwheel.js
@@ -401,56 +401,10 @@
         .on('dragend', dragend)
     );
 
-    // Add theme UI
-    var theme = self.container.append('div').attr('class', 'theme');
-
-    var swatches = theme.selectAll('div').data(data);
-    swatches.enter().append('div')
-      .attr('class', 'swatch')
-      .append('div')
-        .attr('class', 'color');
-    swatches.exit().remove();
-
-    // Add sliders
-    var sliders = theme.selectAll('.swatch')
-      .append('input')
-      .attr('type', 'range')
-      .attr('class', 'slider')
-      .on('input', function (d) {
-        d.v = parseInt(this.value) / 100;
-        updateTheme();
-      });
-
-    // Add color codes
-    var colorValues = theme.selectAll('.swatch')
-      .append('input')
-        .attr('type', 'text')
-        .attr('class', 'value')
-        .on('focus', function () {
-          // Like jQuery's .one(), attach a listener that only executes once.
-          // This way the user can use the cursor normally after the initial selection.
-          d3.select(this).on('mouseup', function () {
-            d3.event.preventDefault();
-            // Detach the listener
-            d3.select(this).on('mouseup', null);
-          })
-          this.select();
-        });
-
-    // Add mode toggle UI
-    var modeToggle = self.container.append('select')
-      .attr('class', 'mode-toggle')
-      .on('change', function () {
-        self.currentMode = this.value;
-        init();
-      });
-
-    for (var mode in modes) {
-      modeToggle.append('option').text(modes[mode])
-        .attr('selected', function () {
-          return modes[mode] == self.currentMode ? 'selected' : null;
-        });
-    }
+    // plugins
+    ColorWheel.plugins.forEach(function (plugin) {
+      plugin(self, data);
+    });
 
     // phew
     init();
@@ -488,6 +442,12 @@
   };
 
   ColorWheel.modes = modes;
+
+  ColorWheel.plugins = [];
+
+  ColorWheel.extend = function (plugin) {
+    this.plugins.push(plugin);
+  }
 
   return ColorWheel;
 }));
---
+++ plugins.js
@@ -0,0 +1,54 @@
+ColorWheel.extend(function (wheel, data) {
+  // Add theme UI
+  var theme = wheel.container.append('div').attr('class', 'theme');
+
+  var swatches = theme.selectAll('div').data(data);
+  swatches.enter().append('div')
+    .attr('class', 'swatch')
+    .append('div')
+      .attr('class', 'color');
+  swatches.exit().remove();
+
+  // Add sliders
+  var sliders = theme.selectAll('.swatch')
+    .append('input')
+    .attr('type', 'range')
+    .attr('class', 'slider')
+    .on('input', function (d) {
+      d.v = parseInt(this.value) / 100;
+      updateTheme();
+    });
+
+  // Add color codes
+  var colorValues = theme.selectAll('.swatch')
+    .append('input')
+      .attr('type', 'text')
+      .attr('class', 'value')
+      .on('focus', function () {
+        // Like jQuery's .one(), attach a listener that only executes once.
+        // This way the user can use the cursor normally after the initial selection.
+        d3.select(this).on('mouseup', function () {
+          d3.event.preventDefault();
+          // Detach the listener
+          d3.select(this).on('mouseup', null);
+        })
+        this.select();
+      });
+});
+
+ColorWheel.extend(function (wheel) {
+  // Add mode toggle UI
+  var modeToggle = wheel.container.append('select')
+    .attr('class', 'mode-toggle')
+    .on('change', function () {
+      wheel.currentMode = this.value;
+      init();
+    });
+
+  for (var mode in ColorWheel.modes) {
+    modeToggle.append('option').text(ColorWheel.modes[mode])
+      .attr('selected', function () {
+        return ColorWheel.modes[mode] == wheel.currentMode ? 'selected' : null;
+      });
+  }
+});
